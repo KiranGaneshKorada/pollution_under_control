a98c77a3888edbf8b282ae3fc2da425111cf17c6
Kiran Ganesh Korada <kiranganeshkorada@gmail.com>
2023-11-05 16:49:02 +0530

    debud true

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ set -o errexit  # exit on error
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+
 python manage.py migrate
--- pollution_under_control/settings.py
+++ pollution_under_control/settings.py
@@ -129,13 +129,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-if DEBUG:
-        STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
-else:
-        STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 MEDIA_URL='media/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
