f3461128caa14bd5ab1dab6a33eccd347147d385
Kiran Ganesh Korada <kiranganeshkorada@gmail.com>
2023-11-05 21:41:15 +0530

    ...

--- pollution_under_control/settings.py
+++ pollution_under_control/settings.py
@@ -81,18 +81,18 @@ WSGI_APPLICATION = 'pollution_under_control.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME':'pucdb',
-#         'USER':'root',
-#         'PASSWORD':'likkiran8Rslk4'
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'pucdb',
+        'USER':'root',
+        'PASSWORD':'likkiran8Rslk4'
+    }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+# }
 
 
 # Password validation
