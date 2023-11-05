73747a7cecf4439d9737b3ff85ad7c16be9d667d
Kiran Ganesh Korada <kiranganeshkorada@gmail.com>
2023-11-05 16:46:27 +0530

    static debuc from stackof

--- pollution_under_control/settings.py
+++ pollution_under_control/settings.py
@@ -130,10 +130,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 MEDIA_URL='media/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+STATIC_ROOT = 'static/'
+
 
 
 # Default primary key field type
